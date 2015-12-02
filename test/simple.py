627f3857e52737a6cc016b7580e2d22b8605f7f2
Bing Sun <subi.the.dream.walker@gmail.com>
2015-12-02 20:48:40 +0800

    fix test/simple.py

--- test/simple.py
+++ test/simple.py
@@ -1,6 +1,8 @@
+from config import *
+
 run([
     ('Basic label', label('Hello world!')),
-    ('Basic label2', label('Hello world!'))),
+    ('Basic label2', label('Hello world!')),
     ('Label with attr', m('label', '<b>Bold and indent</b>', {'indent': 20})),
-    ('Nested label', m('label', 'outer', m('label', 'inner'))),
+    ('Nested label', m('label', 'parent', m('label', '      child'))),
 ])
