a9c825dd080979aba2dddb26b8418dcd6b2cec05
Bing Sun <subi.the.dream.walker@gmail.com>
2015-12-02 21:55:58 +0800

    add test against addWidget()

--- test/layout.py
+++ test/layout.py
@@ -1,5 +1,7 @@
 from config import *
 
+from PyQt5.QtCore import Qt
+
 run([
     ('H', m('Widget', [label('1'), label('2'), label('3')])),
     ('V', m('Widget', (label('1'), label('2'), label('3')))),
@@ -21,6 +23,9 @@ run([
         (label('4'), label('5'), label('6')),
     ))),
 
+    ('H+item', m('Widget', [label('1'), 'stretch', label('2'), label('3')])),
+    ('H+item(arg)', m('Widget', ['stretch', label('1'), {'spacing': 80}, label('2'), label('3')])),
+
     ## ambiguous: setSpacing or addSpacing?
     #('H+config', m('Widget', [{'spacing': 20,}])),
 
@@ -37,5 +42,11 @@ run([
     ])),
 
     ('H+item', m('Widget', [label('1'), 'stretch', label('2'), label('3')])),
-    ('H+item(arg)', m('Widget', ['stretch', label('1'), {'spacing': 80}, label('2'), label('3')])),
+
+    ('H+widget', m('Widget', [
+        {'widget': (label('1'), {'alignment': Qt.AlignRight})},
+        'stretch',
+        label('2'),
+        {'widget': (label('3'), 1, Qt.AlignJustify)}
+    ])),
 ])
