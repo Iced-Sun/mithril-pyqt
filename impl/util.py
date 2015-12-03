cfd37a75fa2d3845aa54379937db172ee46dd57a
Bing Sun <subi.the.dream.walker@gmail.com>
2015-12-03 16:10:56 +0800

    add m.add() function to conveniently implement add_*

---
+++ impl/util.py
@@ -0,0 +1,27 @@
+class _Forwarder(object):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        pass
+
+    def apply(self, f):
+        return f(*self.args, **self.kwargs)
+
+    pass
+
+def forward(*args, **kwargs):
+    return _Forwarder(*args, **kwargs)
+
+class _Adder(object):
+    def __init__(self, target, *args, **kwargs):
+        self.forwarder = forward(*args, **kwargs)
+        self.target = target
+        pass
+
+    def apply(self):
+        return self.forwarder.apply(self.target)
+
+    pass
+
+def add(target, *args, **kwargs):
+    return _Adder(target, *args, **kwargs)
--- mithril.py
+++ mithril.py
@@ -7,7 +7,7 @@ if cmd_folder not in sys.path:
     pass
 
 # use this if you want to include modules from a subfolder
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"subfolder")))
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"impl")))
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
     pass
@@ -22,3 +22,7 @@ m.prop = impl.prop.prop
 
 import impl.query
 m.get_element_by_id = impl.query.get_element_by_id
+
+import impl.util
+m.forward = impl.util.forward
+m.add = impl.util.add
