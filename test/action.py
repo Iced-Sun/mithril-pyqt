923025945118edbb98b456ef456d95a25fa9e189
Bing Sun <subi.the.dream.walker@gmail.com>
2015-12-03 17:36:37 +0800

    clean-up of test/action.py

--- test/action.py
+++ test/action.py
@@ -1,34 +1,27 @@
 from config import *
 
-#return m('main_window', m('menu_bar', m('menu', 'File', m('action', 'action1'))))
-#return m('widget', m('menu_bar', m('menu', 'File', m('action', 'action1'))))
-#return m('widget', m('menu', [m('action', 'action1'), m('action', 'action2')]))
-
 run([
     ('menubar@widget', m('widget', [m('menu_bar', m('menu', 'Menu', m('action', 'Action'))), label('1'), label('2')])),
 
     ('menubar in layout@widget', m('widget', [
         {
             'layout': 'grid_layout',
-            'spacing': 100,
+            'spacing': 20,
             'menu_bar': m('menu_bar', m('menu', 'Menu', m('action', 'Action'))),
         },
-        label('1'),
-        label('2')
+        label('1'), label('2')
     ])),
 
+    ('invisible action@widget', m('widget', m('action', 'Action'))),
     ('invisible menu & action@widget', m('widget', m('menu', 'Menu', m('action', 'Action')))),
-    ('invisible menu & visible action@widget', m('widget', (m('menu', 'Menu', m('action', 'Action')), label('Label')))),
+    ('invisible menu & visible action@widget+layout', m('widget', (m('menu', 'Menu', m('action', 'Action')),))),
 
-    #('action@widget', m('widget', m('action', 'action1'))),
+    #return m('widget', m('menu', [m('action', 'action1'), m('action', 'action2')]))
 
     #('main_window', m('main_window', [label('1'), label('2'), label('3')])),
     #('main_window', m('main_window', {'central_widget': label('central')}, [label('1'), label('2'), label('3')])),
 
     #('menubar+menu@mainwindow', m('main_window', m('menu_bar', m('menu', 'File', m('action', 'action1'))))),
-
-    #('menu', m('widget', m('menu', m('action', 'action1')))),
-    #('mixed action+widget', m('widget', (m('action', 'action2'), label('other')))),
 ])
 
 
