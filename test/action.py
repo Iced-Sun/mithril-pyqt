19a607edf6927a300d3027cfbc9398c430446f53
Bing Sun <subi.the.dream.walker@gmail.com>
2015-12-04 14:57:27 +0800

    clean test/action.py, and make it clear that what we don't support

--- test/action.py
+++ test/action.py
@@ -6,7 +6,6 @@ run([
     ('invisible menu & visible action@widget+layout', m('widget', (m('menu', 'Menu', m('action', 'Action')),))),
 
     ('menubar@widget', m('widget', [m('menu_bar', m('menu', 'Menu', m('action', 'Action'))), label('1'), label('2')])),
-
     ('menubar in layout@widget', m('widget', [
         {
             'layout': 'grid_layout',
@@ -34,20 +33,32 @@ run([
     ('nested menu', m('widget', [
         {
             'menu_bar': m('menu_bar', m('menu', 'Menu', [
-                m('action', 'Auto A'),
+                m('action', 'Auto Action'),
+                m('menu',   'Auto Menu', m('action', 'Auto Action')),
+                m('menu',   'Auto Menu', [m('action', 'Auto Action'), m('action', 'Auto Action')]),
                 'separator',
-                m.add('menu', m('menu', 'Adder menu A', m('action', 'Auto B'))),
-                m.add('menu', 'Adder menu B'),
-                ## FIXME should support this?
-                #m('menu', 'Auto menu', m.add('action', 'Auto C')),
-                m('menu', 'Auto menu', m('action', 'Auto C')),
-
-                m('menu', 'Auto menu with multiple actions', [m('action', 'Auto D'), m('action', 'Auto E')]),
+                m.add('menu', 'Adder Menu'),
+                ## we don't support add_*(m()), because it's just both ugly and subtle
+                #m.add('action', m('action', 'Adder Action'))
+                #m.add('menu', m('menu', 'Adder Menu'))
             ])),
         },
     ])),
+])
 
+'''
     ('menu with action group', m('widget', [
+        {
+            'menu_bar': m('menu_bar', m('menu', 'Menu', [
+                m('action', 'Auto A'),
+                'separator',
+                (m('action', 'Auto B-1'), m('action', 'Auto B-2')),
+                'separator',
+                [m('action', 'Auto C-1'), m('action', 'Auto C-2')],
+                'separator',
+                m('action_group', [m('action', 'Auto D-1'), m('action', 'Auto D-2')])
+            ])),
+        }
     ]))
 
     #('', m('widget', m('menu', [m('action', 'Action A'), m('action', 'Action B'), 'Action C'])
@@ -57,8 +68,9 @@ run([
     #('main_window', m('main_window', {'central_widget': label('central')}, [label('1'), label('2'), label('3')])),
 
     #('menubar+menu@mainwindow', m('main_window', m('menu_bar', m('menu', 'File', m('action', 'action1'))))),
-])
 
+
+'''
 
 '''
 we have three relationships to support: parent-child, add_*, set_*
