78a71623e085a5945d907772ede2f7fe88e8e10f
Bing Sun <subi.the.dream.walker@gmail.com>
2015-12-04 12:11:59 +0800

    add type tag test for m()

--- test/simple.py
+++ test/simple.py
@@ -1,8 +1,11 @@
 from config import *
+from PyQt5.QtWidgets import QLabel
 
 run([
     ('Basic label', label('Hello world!')),
     ('Basic label2', label('Hello world!')),
     ('Label with attr', m('label', '<b>Bold and indent</b>', {'indent': 20})),
     ('Nested label', m('label', 'parent', m('label', '      child'))),
+
+    ('tag type', m(QLabel, 'Hello world!'))
 ])
