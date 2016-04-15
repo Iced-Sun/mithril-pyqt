e32da0401413545e00a3926fe351909b16a71e22
Bing Sun <subi.the.dream.walker@gmail.com>
2016-04-16 00:14:37 +0800

    test/form.py: add complex examples

--- test/form.py
+++ test/form.py
@@ -13,6 +13,9 @@ run([
         m.add('row', 'HBox',       [m('line_edit'), m('line_edit')]),
         m.add('row', 'VBox',       (m('line_edit'), m('line_edit'))),
         m.add('row', 'HVBox',      [(m('line_edit'), m('line_edit')), (m('line_edit'), m('line_edit'))]),
+
+        m.add('row', 'HBox with adder',  [m('line_edit'), 'stretch', m('line_edit'), m.add('spacing', 30)]),
+        m.add('row', 'HVBox with adder', [(m('line_edit'), m.add('spacing', 30), m('line_edit')), 'stretch', (m('line_edit'), m('line_edit'))]),
     ])),
 ])
 
