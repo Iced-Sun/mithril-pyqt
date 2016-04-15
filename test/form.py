6e4bedbd898c039681e9f1887de0290d0fb1b58a
Bing Sun <subi.the.dream.walker@gmail.com>
2016-04-15 23:21:10 +0800

    test/form.py: add('row', cell, cell) now works

--- test/form.py
+++ test/form.py
@@ -1,16 +1,16 @@
 from config import *
-
 run([
     ('Form', m('widget', [
         {
             'layout': 'form'
         },
 
-        label('1'),
-        #m.add('row', 'Name',       m('line_edit')),
-        #m.add('row', label('Age'), m('spin_box')),
-        #m.add('row', 'VBox',       (m('line_edit'), m('line_edit'))),
+        label('1'),               # addWidget
+        m.add('row', label('1')), # addRow
+
+        m.add('row', 'Name',       m('line_edit')),
+        m.add('row', label('Age'), m('spin_box')),
         #m.add('row', 'HBox',       [m('line_edit'), m('line_edit')]),
-        #m.add('row', m('text_edit'))
+        #m.add('row', 'VBox',       (m('line_edit'), m('line_edit'))),
     ])),
 ])
