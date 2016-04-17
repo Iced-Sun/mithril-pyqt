c6191bd2187437030d31345c556cd16c41610791
Bing Sun <subi.the.dream.walker@gmail.com>
2016-04-17 20:22:35 +0800

    test/signal.py: clean the code

--- test/signal.py
+++ test/signal.py
@@ -1,12 +1,11 @@
 from config import *
 
 _text = '1111'
-def text(val=None):
+def print_text(val):
     global _text
-    if val is not None:
-        _text = val
-        print(val)
-    return _text
+    _text = val
+    print(_text)
+    return
 
 from PyQt5.QtCore import QObject, pyqtSignal
 class Controller(QObject):
@@ -25,21 +24,33 @@ class Controller(QObject):
 ctrl = Controller()
 
 run([
-    ('standard output', m('Widget', [m('label', text()), m('line_edit', {'on_text_edited': text})])),
+    ('standard output', m('Widget', [
+        label(_text),
+        m('line_edit', {'on_text_edited': print_text})
+    ])),
 
-    ('connect signal by id (programmable)', m('Widget', [m('label#label1', 'init text'), m('line_edit', {
-        'on_text_edited': lambda text: m.get_element_by_id('label1').setText(text)
-    })])),
+    ('connect signal by id (programmable)', m('Widget', [
+        m('label#label1', 'init text'),
+        m('line_edit', {'on_text_edited': lambda text: m.get_element_by_id('label1').setText(text)})
+    ])),
 
-    ('connect signal by id (declarative)', m('Widget', [m('label#label2', 'init text'), m('line_edit', {
-        'on_text_edited': {'selector': '#label2', 'slot': 'set_text'}
-    })])),
+    ('connect signal by id (declarative)', m('Widget', [
+        m('label#label2', 'init text'),
+        m('line_edit', {'on_text_edited': {'selector': '#label2', 'slot': 'set_text'}})
+    ])),
 
-    ('connect signal by id (shortcut)', m('Widget', [m('label#label3', 'init text'), m('line_edit', {
-        'on_text_edited': {'slot': '#label3::set_text'}
-    })])),
+    ('connect signal by id (shortcut)', m('Widget', [
+        m('label#label3', 'init text'),
+        m('line_edit', {'on_text_edited': {'slot': '#label3::set_text'}})
+    ])),
 
-    ('connect by intermediate signal', m('Widget', [m('label', {'set_text_on': ctrl.text_changed}), m('line_edit', {
-        'on_text_edited': ctrl.set_text
-    })]))
+    ('connect signal by id (flatten shortcut)', m('Widget', [
+        m('label#label4', 'init text'),
+        m('line_edit', {'on_text_edited': '#label4::set_text'})
+    ])),
+
+    ('connect by intermediate signal', m('Widget', [
+        m('label', {'set_text_on': ctrl.text_changed}),
+        m('line_edit', {'on_text_edited': ctrl.set_text})
+    ]))
 ])
